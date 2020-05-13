37d9ef8860bc06d3b590087b72cc04105013701d
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2020-05-13 09:51:43 -0400

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -10,3 +10,26 @@ const options = {
 
 Rox.register('default', Flags);
 Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
+import Rox from 'rox-browser'
+
+export const Flags = {
+  sidebar: new Rox.Flag(false),
+  title: new Rox.Flag(false)
+};
+
+export const configurationFetchedHandler = fetcherResults => {
+  console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
+  if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
+    window.location.reload(false)
+  }
+  else if (fetcherResults.fetcherStatus === 'ERROR_FETCH_FAILED') {
+    console.log('Error occured! Details are: ' + fetcherResults.errorDetails)
+  }
+};
+
+const options = {
+  configurationFetchedHandler: configurationFetchedHandler
+};
+
+Rox.register('default', Flags);
+Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
