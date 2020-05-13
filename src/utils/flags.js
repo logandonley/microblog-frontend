5cd66dbf7737a5f92dcecf4fcf47cbb91cf47e2d
Logan Donley <logan.donley@gmail.com>
2020-05-13 10:04:38 -0400

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -5,18 +5,6 @@ export const Flags = {
   title: new Rox.Flag(false)
 };
 
-const options = {
-};
-
-Rox.register('default', Flags);
-Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
-import Rox from 'rox-browser'
-
-export const Flags = {
-  sidebar: new Rox.Flag(false),
-  title: new Rox.Flag(false)
-};
-
 export const configurationFetchedHandler = fetcherResults => {
   console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
